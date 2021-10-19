0b32fc9ac1732faf370ecdf959c4a08421821175
Jonathan Bardo <jbardo@godaddy.com>
2021-10-19 13:48:45 -0400

    Add buffer space and specify utf8 prop

--- src/curl.js
+++ src/curl.js
@@ -2,5 +2,12 @@ const { execSync } = require('child_process')
 
 // This is based on circle-github-bot and should be changed
 exports.curl = function curl (url, params = '') {
-  return execSync(`curl -L --silent ${params} "${url}"`, { stdio: 'ignore' }).toString('utf8').trim()
+  return execSync(
+    `curl -L --silent ${params} "${url}"`,
+    {
+      stdio: 'ignore',
+      encoding: 'utf8',
+      maxBuffer: 50 * 1024 * 1024
+    }
+    ).toString('utf8').trim()
 }
